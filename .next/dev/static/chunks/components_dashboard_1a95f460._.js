851a06f9092134fababb86e2c5c3b97ddb1eaa11
tbenjamin1 <tbenpollyl@gmail.com>
2026-04-07 19:15:27 +0200

    your message

--- .next/dev/static/chunks/components_dashboard_1a95f460._.js
+++ .next/dev/static/chunks/components_dashboard_1a95f460._.js
@@ -1056,12 +1056,10 @@ function DashboardLayout() {
                         columnNumber: 16
                     }, this);
                 case "companies":
-                    return /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$compiled$2f$react$2f$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["jsxDEV"])(__TURBOPACK__imported__module__$5b$project$5d2f$components$2f$dashboard$2f$tabs$2f$companies$2d$overview$2e$tsx__$5b$app$2d$client$5d$__$28$ecmascript$29$__["default"], {
-                        onNavigate: setActiveTab
-                    }, void 0, false, {
+                    return /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$compiled$2f$react$2f$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["jsxDEV"])(__TURBOPACK__imported__module__$5b$project$5d2f$components$2f$dashboard$2f$tabs$2f$companies$2d$overview$2e$tsx__$5b$app$2d$client$5d$__$28$ecmascript$29$__["default"], {}, void 0, false, {
                         fileName: "[project]/components/dashboard/dashboard-layout.tsx",
                         lineNumber: 120,
-                        columnNumber: 16
+                        columnNumber: 15
                     }, this);
                 case "consolidated-income":
                     return /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$compiled$2f$react$2f$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["jsxDEV"])(__TURBOPACK__imported__module__$5b$project$5d2f$components$2f$dashboard$2f$tabs$2f$consolidated$2d$income$2e$tsx__$5b$app$2d$client$5d$__$28$ecmascript$29$__["ConsolidatedIncome"], {}, void 0, false, {
